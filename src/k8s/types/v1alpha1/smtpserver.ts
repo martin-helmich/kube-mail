b6d4557f97ba353ba3ea9ae685450362e92f43af
Martin Helmich <m.helmich@mittwald.de>
2020-08-24 13:47:32 +0200

    "string" vs string (#3)

--- src/k8s/types/v1alpha1/smtpserver.ts
+++ src/k8s/types/v1alpha1/smtpserver.ts
@@ -1,7 +1,7 @@
 import {MetadataObject} from "@mittwald/kubernetes/types/meta";
 
 export type SMTPServerSpec = {
-    server: "string";
+    server: string;
     port?: number;
     tls?: boolean;
     authType?: "PLAIN" | "LOGIN" | "CRAM-MD5" | "SCRAM-SHA-1";
