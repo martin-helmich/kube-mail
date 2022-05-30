6d3273fcfd0ea4a2720bc9b508b85789b512ff02
Martin Helmich <m.helmich@mittwald.de>
2022-05-30 18:32:17 +0200

    Optimize Pod Informer for memory consumption

--- src/k8s/pod_store.ts
+++ src/k8s/pod_store.ts
@@ -1,16 +1,12 @@
 import {Pod, PodWithStatus} from "@mittwald/kubernetes/types/core/v1";
-import {Store} from "./store";
 import {INamespacedResourceClient} from "@mittwald/kubernetes";
+import {Store} from "@mittwald/kubernetes/cache";
 
-export class PodStore implements Store<PodWithStatus> {
+class PodStoreCacheSet {
     private podsByIP = new Map<string, PodWithStatus>();
     private podsByName = new Map<string, PodWithStatus>();
 
-    public constructor(private inner: Store<PodWithStatus>, private podClient: INamespacedResourceClient<Pod, any, any, PodWithStatus>) {
-    }
-
-
-    public store(obj: PodWithStatus): void {
+    public store(obj: PodWithStatus) {
         this.podsByName.set(`${obj.metadata.namespace}/${obj.metadata.name}`, obj);
 
         if (obj.status.podIP) {
@@ -18,8 +14,55 @@ export class PodStore implements Store<PodWithStatus> {
         }
     }
 
+    public pull(obj: PodWithStatus) {
+        this.podsByName.delete(`${obj.metadata.namespace}/${obj.metadata.name}`);
+        this.podsByIP.delete(obj.status.podIP);
+    }
+
+    public getByName(namespace: string, name: string): PodWithStatus | undefined {
+        return this.podsByName.get(`${namespace}/${name}`);
+    }
+
+    public getByIP(ip: string): PodWithStatus | undefined {
+        return this.podsByIP.get(ip);
+    }
+}
+
+export type PodPredicate = (pod: PodWithStatus) => boolean
+
+export const PodPredicates = {
+    Any: (p: PodWithStatus) => true,
+    OnlyRunning: (p: PodWithStatus) => p.status.phase === "Running",
+}
+
+export class PodStore implements Store<PodWithStatus> {
+    private cache = new PodStoreCacheSet();
+    private readonly predicate: PodPredicate;
+
+    public constructor(private inner: Store<PodWithStatus>, private podClient: INamespacedResourceClient<Pod, any, any, PodWithStatus>, predicate = PodPredicates.Any) {
+        this.predicate = predicate;
+    }
+
+    public async store(obj: PodWithStatus): Promise<void> {
+        if (this.predicate(obj)) {
+            this.cache.store(obj);
+        } else {
+            this.cache.pull(obj);
+        }
+    }
+
+    public async sync(pods: PodWithStatus[]): Promise<void> {
+        const newCache = new PodStoreCacheSet()
+
+        for (const pod of pods.filter(this.predicate)) {
+            newCache.store(pod);
+        }
+
+        this.cache = newCache;
+    }
+
     public async get(namespace: string, name: string): Promise<PodWithStatus | undefined> {
-        const fromCache = this.podsByName.get(namespace + "/" + name);
+        const fromCache = this.cache.getByName(namespace, name);
         if (fromCache) {
             return fromCache;
         }
@@ -32,8 +75,8 @@ export class PodStore implements Store<PodWithStatus> {
         return fromInner;
     }
 
-    public async getByPodIP(podIP: string): Promise<PodWithStatus|undefined> {
-        const fromCache = this.podsByIP.get(podIP);
+    public async getByPodIP(podIP: string): Promise<PodWithStatus | undefined> {
+        const fromCache = this.cache.getByIP(podIP);
         if (fromCache) {
             return fromCache;
         }
@@ -48,8 +91,7 @@ export class PodStore implements Store<PodWithStatus> {
         return pod;
     }
 
-    public pull(obj: PodWithStatus): void {
-        this.podsByName.delete(`${obj.metadata.namespace}/${obj.metadata.name}`);
-        this.podsByIP.delete(obj.status.podIP);
+    public async pull(obj: PodWithStatus): Promise<void> {
+        this.cache.pull(obj);
     }
 }
